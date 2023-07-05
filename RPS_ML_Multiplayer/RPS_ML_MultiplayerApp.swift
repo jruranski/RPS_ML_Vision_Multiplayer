d7091c20c2a3bcfdf4fe1177a7fde8a9abad2f84
Jacob <j.ruranski@icloud.com>
2023-07-05 15:48:23 +0200

    Initial Commit

---
+++ RPS_ML_Multiplayer/RPS_ML_MultiplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_ML_MultiplayerApp.swift
+//  RPS_ML_Multiplayer
+//
+//  Created by Jakub Ruranski on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RPS_ML_MultiplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
